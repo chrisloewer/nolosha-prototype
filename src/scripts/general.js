232777b8542e131e7a7d33f54f64eed8ff456a71
chris <chris.loewer@gmail.com>
2020-03-28 15:19:06 -0700

    Add slideout mobile menu drawer.

--- src/scripts/general.js
+++ src/scripts/general.js
@@ -20,26 +20,26 @@ window.addEventListener('load', function () {
 
 function toggleMenuPopup() {
   let body = document.getElementById('body');
-  // let menu = document.getElementById('menu-popup');
+  let menu = document.getElementById('mobile-menu-drawer');
   let icon = document.getElementById('hamburger-icon-body');
   if (body && icon) {
     toggleClass(body, 'scroll-lock');
-    // toggleClass(menu, 'open');
-    toggleClass(icon, 'open');
+    toggleClass(menu, 'mobile-menu--open');
+    toggleClass(icon, 'hamburger-icon--open');
   }
 }
 
 function closeMenuPopup() {
-  let menu = document.getElementById('menu-popup');
+  let menu = document.getElementById('mobile-menu-drawer');
   if (menu) {
-    removeClass(menu, 'open');
+    removeClass(menu, 'mobile-menu--open');
   }
 }
 
 function openMenuPopup() {
-  let menu = document.getElementById('menu-popup');
+  let menu = document.getElementById('mobile-menu-drawer');
   if (menu) {
-    addClass(menu, 'open');
+    addClass(menu, 'mobile-menu--open');
   }
 }
 
